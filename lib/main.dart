cf3c6c50af8cca13562e4c3133d3b3cfa5861667
arroyostack <arroyostack@gmail.com>
2024-02-10 15:00:22 +0100

    Feat: Basic app structure.

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// Need to import a hlper library
+// from Flutter to get content
+import 'package:flutter/material.dart';
+
+// Define a main function to run when our app starts
+void main() {
+// Create a new text widget to show tex
+  const app = MaterialApp(
+    home: Text('Hi there Comodo!'),
+  );
+
+// Take the widget and get it on the scren.
+  runApp(app);
+}
