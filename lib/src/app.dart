fc5303f1e5eebd8951c7a23d3f685086bd777b7a
arroyostack <arroyostack@gmail.com>
2024-02-11 21:40:50 +0100

    Feat: fetch image form jsonplaceholder with 'http' package + Create image object from ImageModel class and add it to images list.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,10 @@
 // https://jsonplaceholder.typicode.com/photos/1
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'package:flutter_application_1/src/models/image_model.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -11,6 +15,19 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    // Make a request to an
+    // https://jsonplaceholder.typicode.com/photos/1
+    counter += 1;
+    var response = await get(
+        Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter'));
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +37,10 @@ class AppState extends State<App> {
           child: Text('$counter'),
         ),
         floatingActionButton: FloatingActionButton(
+          onPressed: fetchImage,
           child: const Icon(
             Icons.add,
           ),
-          onPressed: () {
-            setState(() {
-              // Increment counter
-              counter += 1;
-            });
-          },
         ),
         appBar: AppBar(
           title: const Text('Hello World'),
