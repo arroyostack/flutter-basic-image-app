93ebf1c749f5270d404c526a31a3ff947727ae7b
arroyostack <arroyostack@gmail.com>
2024-02-11 14:53:13 +0100

    Refactor: Main app class to src/ directory.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(
-          Icons.add,
-        ),
-        onPressed: () {
-          print("Hi there");
-        },
-      ),
-      appBar: AppBar(
-        title: const Text('Hello World'),
-        backgroundColor: Colors.red,
-      ),
-    ),
-  );
-  runApp(app);
+  runApp(const App());
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,1 +1,25 @@
 import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(
+            Icons.add,
+          ),
+          onPressed: () {
+            print("Hi there");
+          },
+        ),
+        appBar: AppBar(
+          title: const Text('Hello World'),
+          backgroundColor: Colors.red,
+        ),
+      ),
+    );
+  }
+}
