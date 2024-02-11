cd11a70fa2be340e862372328a179ca09a6d58d4
arroyostack <arroyostack@gmail.com>
2024-02-11 17:29:50 +0100

    Refactor: class 'App' to statefull widget. Added counter functionality and display.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
-  const App({super.key});
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  AppState createState() => AppState();
+}
+
+class AppState extends State<App> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        body: Center(
+          child: Text('$counter'),
+        ),
         floatingActionButton: FloatingActionButton(
           child: const Icon(
             Icons.add,
           ),
           onPressed: () {
-            print("Image");
+            setState(() {
+              counter += 1;
+            });
           },
         ),
         appBar: AppBar(
